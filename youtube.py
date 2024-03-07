242650e0e313de2bbd8406940b94a2c1134c41e0
Sasoed2020 <119231767+Sasoed2020@users.noreply.github.com>
2024-03-07 20:33:09 +0200

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -3,9 +3,11 @@ from telebot import TeleBot, types
 from pytube import YouTube
 from io import BytesIO
 
-# Замените 'YOUR_BOT_TOKEN' на токен вашего бота
-TOKEN = '6063603462:AAFLehcN0lkP6iDVqfEGdMWHgstrUk4aJc0'
+TOKEN = 'YOUR_BOT_TOKEN'
 bot = TeleBot(TOKEN)
+
+def sanitize_filename(filename):
+    return "".join([c for c in filename if c.isalpha() or c.isdigit() or c==' ']).rstrip()
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -16,9 +18,13 @@ def echo_all(message):
     url = message.text
     try:
         yt = YouTube(url)
-        title = yt.title
-        thumbnail_url = yt.thumbnail_url
+        title = sanitize_filename(yt.title)
 
+        # Создаем директорию с названием видео, если она не существует
+        if not os.path.exists(title):
+            os.makedirs(title)
+
+        thumbnail_url = yt.thumbnail_url
         markup = types.InlineKeyboardMarkup()
         video_btn = types.InlineKeyboardButton('Видео', callback_data='video_' + url)
         audio_btn = types.InlineKeyboardButton('Аудио', callback_data='audio_' + url)
@@ -31,27 +37,26 @@ def echo_all(message):
 def download_send_video(call):
     url = call.data.split('_')[1]
     yt = YouTube(url)
+    title = sanitize_filename(yt.title)
     video = yt.streams.get_highest_resolution()
-    # Указываем путь для временного сохранения видео
-    download_path = "./temp_video.mp4"
-    video.download(filename=download_path)
-    with open(download_path, 'rb') as video_file:
-        bot.send_video(call.message.chat.id, video=video_file)
-    # Удаляем файл после отправки
-    os.remove(download_path)
+
+    video_path = os.path.join(title, 'video.mp4')
+    if not os.path.exists(video_path):
+        video.download(output_path=title, filename='video.mp4')
+
+    bot.send_video(call.message.chat.id, open(video_path, 'rb'))
 
 def download_send_audio(call):
     url = call.data.split('_')[1]
     yt = YouTube(url)
+    title = sanitize_filename(yt.title)
     audio = yt.streams.get_audio_only()
-    # Указываем путь для временного сохранения аудио
-    download_path = "./temp_audio.mp3"
-    audio.download(filename=download_path)
-    with open(download_path, 'rb') as audio_file:
-        bot.send_audio(call.message.chat.id, audio=audio_file)
-    # Удаляем файл после отправки
-    os.remove(download_path)
 
+    audio_path = os.path.join(title, 'audio.mp3')
+    if not os.path.exists(audio_path):
+        audio.download(output_path=title, filename='audio.mp3')
+
+    bot.send_audio(call.message.chat.id, open(audio_path, 'rb'))
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
